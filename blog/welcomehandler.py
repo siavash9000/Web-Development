656817757193cbc674a25ab26c214bffdab81d11
siavash <siavash@siavashs-iMac.local>
2014-03-30 13:56:21 +0200

    fixed minor issues of cookie handling

--- blog/welcomehandler.py
+++ blog/welcomehandler.py
@@ -6,7 +6,15 @@ class WelcomeHandler(webapp2.RequestHandler):
     hasher = hasher()
     def get(self):
         userid_cookie = self.request.cookies.get('userid')
-        userid = int(userid_cookie.split("|")[0])  
-        user = User.get_by_id(userid)
-        if self.hasher.check_secure_val(userid_cookie):
-            self.response.write("Welcome "+user.username+" !")
+        if userid_cookie and len(userid_cookie) > 0:
+            userid = int(userid_cookie.split("|")[0])  
+            user = User.get_by_id(userid)
+            if not user:
+                self.response.headers.add_header('Set-Cookie','userid=;Path=/')
+                self.response.write("Welcome!")        
+            else:
+                if self.hasher.check_secure_val(userid_cookie):
+                    self.response.write("Welcome "+user.username+" !")
+        else:
+            self.response.write("Welcome Cookie-less Stranger!")
+
